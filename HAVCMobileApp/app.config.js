da3f78dfc7b40365e275bd16ceaba20a3ad79600
IvanQuestMaster <ivan.barbancic@ovi.com>
2014-07-18 17:50:03 +0200

    dodan Modul1

--- HAVCMobileApp/app.config.js
+++ HAVCMobileApp/app.config.js
@@ -1,17 +1,27 @@
-﻿window.HAVCMobileApp = $.extend(true, window.HAVCMobileApp, {
-    "config": {
-        "layoutSet": "navbar",
-        "navigation": [
-            {
-                title: "Home",
-                action: "#home",
-                icon: "home"
-            },
-            {
-                title: "About",
-                action: "#about",
-                icon: "info"
-            }
-        ]
-    }
+window.HAVCMobileApp = $.extend(true, window.HAVCMobileApp, {
+  "config": {
+    "layoutSet": "navbar",
+    "navigation": [
+      {
+        "title": "Home",
+        "action": "#home",
+        "icon": "home"
+      },
+      {
+        "title": "About",
+        "action": "#about",
+        "icon": "info"
+      },
+      {
+        "title": "Modul1",
+        "action": "#Modul1",
+        "icon": "modul1"
+      },
+      {
+        "title": "View1",
+        "action": "#View1",
+        "icon": "view1"
+      }
+    ]
+  }
 });
